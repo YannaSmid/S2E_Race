fe5d89968adb051fd1f0ea4e4fbb315d79dd8430
YannaSmid <yanna.smid@gmail.com>
2024-06-06 13:35:37 +0200

    COLLECTING COINS

--- Racing/Assets/CollectCoin.cs
+++ Racing/Assets/CollectCoin.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CollectCoin : MonoBehaviour
 {
     public bool collected = false;
+    CoinsHandler coinshandler;
+
+    private Transform coinsinfo;
+    public TMP_Text counterText;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        coinsinfo = GameObject.Find("HUD1").transform.Find("CoinsInfo");
+        counterText = coinsinfo.Find("Text").gameObject.GetComponent<TMP_Text>();
+
+        coinshandler = GameObject.Find("CoinsHandler").GetComponent<CoinsHandler>();
     }
 
     // Update is called once per frame
@@ -22,6 +30,8 @@ public class CollectCoin : MonoBehaviour
 
         if (!collected){
             collected = true;
+            coinshandler.totalMoney += 100;
+            counterText.text = coinshandler.totalMoney.ToString();
             Debug.Log(" Collect the coinzzz");
             this.gameObject.SetActive(false);
         }

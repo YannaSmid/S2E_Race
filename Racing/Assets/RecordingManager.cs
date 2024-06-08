57e81115a62bbffa0522490eb13ed95dd47a20c3
YannaSmid <yanna.smid@gmail.com>
2024-06-08 16:10:48 +0200

    recording update

--- Racing/Assets/RecordingManager.cs
+++ Racing/Assets/RecordingManager.cs
@@ -18,11 +18,17 @@ public class RecordingManager : MonoBehaviour
     public Transform kart;
     public Transform ghostKart;
 
-    public bool recording;
-    public bool playing;
+    public Transform kart2;
+    public Transform ghostKart2;
+
+    public bool recording = true;
+    public bool playing = false;
 
     private List<GhostTransform> recordedGhostTransforms = new List<GhostTransform>();
     private GhostTransform lastRecordedGhostTransform;
+
+    private List<GhostTransform> recordedGhostTransforms2 = new List<GhostTransform>();
+    private GhostTransform lastRecordedGhostTransform2;
     // Start is called before the first frame update
     void Start()
     {
@@ -41,6 +47,14 @@ public class RecordingManager : MonoBehaviour
                 lastRecordedGhostTransform = newGhostTransform;
 
             }
+            if(kart2.position != lastRecordedGhostTransform2.position || kart2.rotation != lastRecordedGhostTransform2.rotation){
+
+                var newGhostTransform2 = new GhostTransform(kart2);
+                recordedGhostTransforms2.Add(newGhostTransform2);
+
+                lastRecordedGhostTransform2 = newGhostTransform2;
+
+            }
         }
 
         if (playing){
@@ -51,7 +65,9 @@ public class RecordingManager : MonoBehaviour
     void Play(){
 
         ghostKart.gameObject.SetActive(true);
+        ghostKart2.gameObject.SetActive(true);
         StartCoroutine(StartGhost());
+        StartCoroutine(StartGhost2());
         playing = false;
     }
 
@@ -60,6 +76,18 @@ public class RecordingManager : MonoBehaviour
         for (int i=0; i<recordedGhostTransforms.Count; i++){
             ghostKart.position = recordedGhostTransforms[i].position;
             ghostKart.rotation = recordedGhostTransforms[i].rotation;
+
+            yield return new WaitForFixedUpdate();
+        }
+
+
+    }
+
+    IEnumerator StartGhost2()
+    {
+        for (int i=0; i<recordedGhostTransforms2.Count; i++){
+            ghostKart2.position = recordedGhostTransforms2[i].position;
+            ghostKart2.rotation = recordedGhostTransforms2[i].rotation;
 
             yield return new WaitForFixedUpdate();
         }

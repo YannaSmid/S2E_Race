9a438e8057b7ae1e172b78961add92a463b70051
SanneDekker <sannedekker2@gmail.com>
2024-06-08 14:03:16 +0200

    coin sound toegevoegd

--- Racing/Assets/Karting/Scripts/CollectCoin.cs
+++ Racing/Assets/Karting/Scripts/CollectCoin.cs
@@ -6,14 +6,14 @@ using TMPro;
 public class CollectCoin : MonoBehaviour
 {
     public bool collected = false;
-    CoinsHandler coinshandler;
-
+    private CoinsHandler coinshandler;
     public float rotation_speed = 0.5f;
+    public AudioClip collectSound;
+    public float soundVolume = 0.2f;
 
     // Start is called before the first frame update
     void Start()
     {
-
         coinshandler = GameObject.Find("CoinsHandler").GetComponent<CoinsHandler>();
     }
 
@@ -23,16 +23,24 @@ public class CollectCoin : MonoBehaviour
         this.transform.Rotate(0.0f, 1.0f * rotation_speed, 0.0f, Space.Self);
     }
 
-    void OnTriggerEnter(Collider other){
-
-        if (!collected){
+    void OnTriggerEnter(Collider other)
+    {
+        if (!collected)
+        {
             collected = true;
             coinshandler.totalMoney += 100;
             coinshandler.updateMoney = true;
             //counterText.text = coinshandler.totalMoney.ToString();
-            Debug.Log(" Collect the coinzzz");
+            Debug.Log("Collect the coinzzz");
+
+            GameObject audioObject = new GameObject("CoinSound");
+            AudioSource audioSource = audioObject.AddComponent<AudioSource>();
+            audioSource.clip = collectSound;
+            audioSource.volume = soundVolume;
+            audioSource.Play();
+            Destroy(audioObject, collectSound.length);
+
             this.gameObject.SetActive(false);
         }
-
     }
 }
